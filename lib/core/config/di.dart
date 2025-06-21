89584f9dbf950f7db72a09c07059918d26defa78
Hekmat <m.hekmatfanari@gmail.com>
2025-06-21 15:57:44 +0300

    fix: Remove unused Hive imports in DI configuration
    The `HiveModule` now correctly provides the `Box<UserModel>`, eliminating the need for direct Hive imports (`hive_flutter` and `UserModel`) in `di.dart`.

--- lib/core/config/di.dart
+++ lib/core/config/di.dart
@@ -2,8 +2,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:technical_assessment/core/config/di.config.dart';
-import 'package:technical_assessment/features/auth/login/data/model/user_model.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 final getIt = GetIt.instance;
 
